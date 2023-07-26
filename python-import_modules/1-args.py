69eba2ea3c557a776b39631d8a4c5d49c67f267e
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-07-26 23:06:36 +0300

    task 0

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys 
+
+num = len(sys.argv)
+if num == 0:
+    print("{} arguments.".format(num))
+    print("{}: {}".format(num, sys.argv)) 
+if num == 1:
+    print("{} argument.".format(num))  
+    print("{}: {}".format(num, sys.argv)) 
+else:
+    print("{} argument.".format(num)) 
+    print("{}: {}".format(num, sys.argv)) 
+
+
+    
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+This is learning python-import_modules
