d6ad0c7ceae38aa19ae2b8c2444b4682cdd5efeb
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-02 17:26:12 +0300

    Updated commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,8 +2,8 @@
 def fibonacci_sequence(n):
     sequence = [0, 1]  # Initializing the first two terms of the Fibonacci sequence
 
-    for i in range(2, n):
-        next_term = sequence[-1] + sequence[-2]  # Calculating the next term in the sequence
-        sequence.append(next_term)
-
+    if __name__ =="__main__":
+        for i in range(2, n):
+            next_term = sequence[-1] + sequence[-2]  # Calculating the next term in the sequence
+    sequence.append(next_term)
     return sequence
