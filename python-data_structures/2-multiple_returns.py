6b9428a12c54e9b58b81c4d3642174ba77f386cf
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-03 18:35:35 +0300

    Task 2 updated

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,14 +1,11 @@
 #!usr/bin/ptyhon3
+
 def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
     return length, first_char
 
-# Test
-sentence = "Hello, world!"
-result = multiple_returns(sentence)
-print(result)  # Output: (13, 'H')
 
-empty_sentence = ""
-empty_result = multiple_returns(empty_sentence)
-print(empty_result)  # Output: (0, None)
+sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
