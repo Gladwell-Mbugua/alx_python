36a92166a3551949e8f89c5118527d6b32c28a1e
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-03 18:11:59 +0300

    updated C commit

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -3,6 +3,7 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char != 'c':
+        if char.lower() != 'c':
             new_string += char
     return new_string
+
