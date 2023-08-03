983e4e72ad6c1a30fc49f56b9dd096f2f4432c42
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-03 21:01:30 +0300

    Final commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,7 @@
 #!usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, num in enumerate(row):
-            print(num, end=" ")
-            if i == len(row) - 1:
-                print()
+        for i, element in enumerate(row):
+            print("{:d}".format(element), end=" " if i < len(row) - 1 else '')
+        print()
 
