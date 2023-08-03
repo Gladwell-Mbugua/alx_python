0992a38771e1401e925ce07c56e7612cf33e96fd
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-03 20:33:39 +0300

    Final commit

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,9 +1,2 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-
-# Test
-my_dict = {'name': 'John', 'age': 25}
-update_dictionary(my_dict, 'name', 'Alice')
-update_dictionary(my_dict, 'city', 'New York')
-
-print(my_dict)
