8a44e47310db011805c01c65013666957b470e1c
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-03 20:48:45 +0300

    commit changes

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -4,9 +4,3 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
     return length, first_char
-
-# Test
-sentence = "Hello, world!"
-result = multiple_returns(sentence)
-print("Length: {} - First character: {}".format(result[0], result[1]))
-
