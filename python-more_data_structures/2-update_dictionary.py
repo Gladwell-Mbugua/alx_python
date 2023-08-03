4df0d41b816fc3fe7b109a897e8817655d274bd1
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-03 20:44:21 +0300

    Task 3

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    my_dict = a_dictionary
+    if key in my_dict:
+        my_dict[key] = value
+    else:
+        my_dict[key] = value
+    return my_dict
