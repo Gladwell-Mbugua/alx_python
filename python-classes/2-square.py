c04cd75f342f6739c79f446ee5d94ea997467bf8
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-10 02:04:14 +0300

    Final commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''
 Write a class Square that defines a square by: (based on 0-square.py)
-
 Private instance attribute: size
 Instantiation with optional size: def __init__(self, size=0):
 size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
@@ -19,7 +18,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >=0")
-        self.size = size 
+        self.__size = size 
 
     def area(self):
-        return self.size ** 2
+        return self.__size ** 2
