74c5f5859a60f21d43f6eabd2379f1b434eb0e0d
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-10 01:00:26 +0300

    Updated Task 1 commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -13,6 +13,14 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
+    "In this implementation, the __init__ method takes an optional parameter size, which is assigned to the private instance attribute __size. It performs the necessary checks to ensure that size is an integer and is greater than or equal to 0, raising appropriate exceptions if the conditions are not met."
+
     def __init__(self, size=0):
-         if not "isintance(type(size), int)":
+        if not isinstance(type(size), int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size 
+
+    
+
