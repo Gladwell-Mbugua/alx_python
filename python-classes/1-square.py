d1cc0d1ba9f6e11f3777e2cfcd01faa6ada2beae
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-10 00:44:19 +0300

    Task 1 Commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Write a class Square that defines a square by:
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+'''
+class Square:
+    "class Square is the blueprint to create a class and an object"
+    " Additionally self, isa keyword that binds the actual value with object"
+    "The self is used to initialize an object and is always called out everytme a new object is created"
+    def __init__(self, size):
+        self.__size = size
+
+    def __init__(self, size=0):
+         if not "isintance(type(size), int)":
+            raise TypeError("size must be an integer")
