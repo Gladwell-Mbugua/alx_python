f8f1937f26ae147e3020af6a6e3b22343bbfe9ae
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-16 22:44:38 +0300

    updated

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -10,3 +10,7 @@ def fibonacci_sequence(n):
         fibonacci_numbers.append(next_number)
     
     return fibonacci_numbers[:n]
+print(fibonacci_sequence(6))
+print(fibonacci_sequence(1))
+print(fibonacci_sequence(0))
+print(fibonacci_sequence(20))
