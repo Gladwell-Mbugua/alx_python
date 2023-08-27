bcc4c8d0602e884f32f7019cea0f7a6863ffe5b4
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-08-27 15:08:25 +0300

    Final commit on Import

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,9 +2,4 @@
 def raise_exception():
     value = "string"
     value += 5  # This will raise a TypeError
-    
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
 
