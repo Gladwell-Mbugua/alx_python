50374de0069a13e497818cbef72418645915123c
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-20 14:02:50 +0300

    Fianl commit for task 3

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env/python3
 """
-Write an empty class BaseGeometry.
-
-You are not allowed to import any module
-"""
-
-#!/usr/bin/python3
-"""
 This is a base class for custom metaclass TypeMetaClass.
 """
 
 class TypeMetaClass(type):
     """
-    This is a metaclass used to represent the class type inorder to eliminate
-    the inherited method init subclass
+    This is a metaclass used to represent the class type in order to eliminate
+    the inherited method init subclass.
     """
     def __dir__(cls) -> None:
         """
-        Exclude attribute init subclass in dir()
+        Exclude attribute init subclass in dir().
         """
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
@@ -27,6 +20,5 @@ class BaseGeometry(metaclass=TypeMetaClass):
     This is a base class that uses the custom metaclass TypeMetaClass to exclude
     the __init_subclass__ attribute from the list of attributes returned by dir().
     """
-    pass
-
-
+    def __init__(self):
+        pass
