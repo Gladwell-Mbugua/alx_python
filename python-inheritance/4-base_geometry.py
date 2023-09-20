70eca2dd4e9c18da356837b365a8d9bfbdda2923
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-20 14:52:50 +0300

    Final commit task 4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env/python3
 """
-Write a class BaseGeometry (based on 3-base_geometry.py).
-
-Public instance method: def area(self): that raises an Exception with the message area() is not implemented
-You are not allowed to import any module
+This is a base class
 """
-class BaseGeometry:
-    """
-    A base geometry class.
-    """
 
+
+class TypeMetaClass(type):
+    """
+    This is a metaclass used to represent the class type inorder to eliminate
+    the inherited method init subclass
+    """
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attributes = super().__dir__()
+
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+
+class BaseGeometry(metaclass=TypeMetaClass):
+    """
+    This is a base class
+    """
     def area(self):
         """
-        Raises an exception indicating that the method is not implemented.
-
-        Raises:
-            Exception: Always raises an exception with the message "area() is not implemented".
+        Public instance method to raise an Exception.
         """
         raise Exception("area() is not implemented")
