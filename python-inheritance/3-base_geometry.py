66f40a2dc66307ddd04a310c52121acae37e65d8
Gladwell Mbugua <gladwellmbugua7@gmail.com>
2023-09-20 13:55:50 +0300

    Final update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,8 +5,11 @@ Write an empty class BaseGeometry.
 You are not allowed to import any module
 """
 
+class TypeMetaClass(type):
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+
 class BaseGeometry(metaclass=TypeMetaClass):
-    """
-    This is a base class
-    """
     pass
+
